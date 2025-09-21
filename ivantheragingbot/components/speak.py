211b56c20a0ada178d82c7a9f47e2ab8f504f3eb
Ivan Hribernik <cihribernik@gmail.com>
2025-09-21 18:46:09 -0300

    fix: scaffold

--- ivantheragingbot/components/speak.py
+++ ivantheragingbot/components/speak.py
@@ -1,5 +1,5 @@
 from twitchio.ext import commands
-from ivantheraginbot.utils import tts
+from ivantheragingbot.utils import tts
 
 
 class SpeakComponent(commands.Component):
@@ -8,6 +8,11 @@ class SpeakComponent(commands.Component):
 
     @commands.command(name="speak")
     @commands.cooldown(rate=1, per=30, key=commands.BucketType.user)
-    async def speak(self, ctx: commands.Context, *, phrase: str):
+    async def speak(self, ctx: commands.Context, *, phrase: str) -> None:
         message = f"{ctx.author.name} dice: {phrase}"
-        await tts(message, self.bot.package_location, self.bot.lang, self.bot.tld)
+        await tts(
+            message,
+            self.bot.package_location,
+            self.bot.lang,
+            self.bot.tld,
+        )
